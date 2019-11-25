9050ff8afc17e87e9e928befeea4b24d47b5d222
Igor Beschastnov <beschastnovigor@gmail.com>
2019-11-25 21:25:07 +0300

    doing something, *hmmm*

---
+++ app/Commands.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Commands where
+
+import           Data.Text                        (Text)
+import qualified Data.Text                        as Text
+
+-- | A help message to show on conversation start with bot.
+startMessage :: Text
+startMessage = Text.unlines
+ [ "Start message."
+ , "Yay!"
+ ]
+
+-- | Message with main server stats - cpu, ram, disk.
+statsMessage :: Text
+statsMessage = Text.unlines
+ [ "Stats message."
+ , "Wow!"
+ ]
--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,54 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import           Control.Applicative              ((<|>))
+import qualified Telegram.Bot.API                 as Telegram
+import           Telegram.Bot.Simple
+import           Telegram.Bot.Simple.Debug
+import           Telegram.Bot.Simple.UpdateParser
+
+import Commands
+
+-- | Bot conversation state model.
+data Model = Model deriving (Show)
+
+-- | Actions bot can perform.
+data Action
+  = NoAction        -- ^ Perform no action.
+  | Stats           -- ^ Show main machine stats
+  | Start           -- ^ Display start message.
+  deriving (Show)
+
+-- | Bot application.
+bot :: BotApp Model Action
+bot = BotApp
+  { botInitialModel = Model
+  , botAction = flip handleUpdate
+  , botHandler = handleAction
+  , botJobs = []
+  }
+
+-- | Processes incoming 'Telegram.Update's and turns them into 'Action's.
+handleUpdate :: Model -> Telegram.Update -> Maybe Action
+handleUpdate _ = parseUpdate
+    $ Stats <$ command "stats"
+  <|> Start <$ command "start"
+
+handleAction :: Action -> Model -> Eff Action Model
+handleAction action model = case action of
+  NoAction -> pure model
+  Stats -> model <# do
+    replyText statsMessage
+    pure NoAction
+  Start -> model <# do
+    replyText startMessage
+    pure NoAction
+
+run :: Telegram.Token -> IO ()
+run token = do
+  env <- Telegram.defaultTelegramClientEnv token
+  startBot_ (traceBotDefault bot) env
+
 main :: IO ()
-main = putStr "Hello World!"
+main = getEnvToken "TELEMONITOR_TOKEN" >>= run
